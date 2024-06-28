11033164a0346d0e20f55602cd271bdfd438961a
stakada <stakada@student.42tokyo.jp>
2024-06-29 06:37:37 +0900

    add ex09

---
+++ go01/ex09/vendor/piscine/basicatoi2.go
@@ -0,0 +1,19 @@
+package piscine
+
+import (
+	_ "ft"
+	_ "fmt"
+)
+
+func BasicAtoi2(s string) int {
+	var result int
+	num := []rune(s)
+	for i := 0; i < len(num); i++ {
+		if '0' <= num[i] && num[i] <= '9' {
+			result = result * 10 + int(num[i] - '0')
+		} else {
+			return 0
+		}
+	}
+	return result
+}
