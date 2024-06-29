75403e74cbee8ca9e97997cbf90bf153798b97d4
stakada <stakada@student.42tokyo.jp>
2024-06-29 20:23:15 +0900

    add ex03

---
+++ go02/ex03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.RecursivePower(4, 3))
+	fmt.Println(piscine.RecursivePower(4, 0))
+	fmt.Println(piscine.RecursivePower(4, 1))
+	fmt.Println(piscine.RecursivePower(4, -1))
+}
