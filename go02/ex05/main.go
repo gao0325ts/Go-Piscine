10a069e074c58b00b69444d4b2ec300a68cfd21f
stakada <stakada@student.42tokyo.jp>
2024-06-29 20:46:11 +0900

    add ex05

---
+++ go02/ex05/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Sqrt(4))
+	fmt.Println(piscine.Sqrt(3))
+	fmt.Println(piscine.Sqrt(-42))
+	fmt.Println(piscine.Sqrt(-1))
+	fmt.Println(piscine.Sqrt(0))
+	fmt.Println(piscine.Sqrt(1))
+	fmt.Println(piscine.Sqrt(144))
+}
