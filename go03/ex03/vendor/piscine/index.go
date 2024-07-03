fd835dcf24bca0dedac5ad2a57a18514cc199140
Shorei Takada <ts20040325@gmail.com>
2024-07-03 12:58:28 +0900

    go03/ex03: use range instead of len

--- go03/ex03/vendor/piscine/index.go
+++ go03/ex03/vendor/piscine/index.go
@@ -3,9 +3,9 @@ package piscine
 func Index(s string, toFind string) int {
 	runes := []rune(s)
 	toFindRunes := []rune(toFind)
-	for i := 0; i <= len(runes)-len(toFindRunes); i++ {
+	for i, _ := range runes {
 		found := true
-		for j := 0; j < len(toFindRunes); j++ {
+		for j, _ := range toFindRunes {
 			if runes[i+j] != toFindRunes[j] {
 				found = false
 				break
