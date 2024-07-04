d013f7d02a90601afa128e23fd2758640ed24219
stakada <stakada@student.42tokyo.jp>
2024-07-04 13:50:51 +0900

    go01/ex09: use range instead of len

--- go01/ex09/vendor/piscine/basicatoi2.go
+++ go01/ex09/vendor/piscine/basicatoi2.go
@@ -1,14 +1,14 @@
 package piscine
 
 func BasicAtoi2(s string) int {
-	var result int
-	num := []rune(s)
-	for i := 0; i < len(num); i++ {
-		if '0' <= num[i] && num[i] <= '9' {
-			result = result * 10 + int(num[i] - '0')
+	var n int
+	for _, r := range []rune(s) {
+		if '0' <= r && r <= '9' {
+			n = n*10 + int(r-'0')
 		} else {
 			return 0
 		}
 	}
-	return result
+	return n
+
 }
