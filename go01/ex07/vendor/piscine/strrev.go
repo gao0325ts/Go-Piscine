800e3258bc77c8fea9bbc03cb0d3638db6647a98
stakada <stakada@student.42tokyo.jp>
2024-07-04 13:25:03 +0900

    go01/ex07: len -> StrLen, use par assign

--- go01/ex07/vendor/piscine/strrev.go
+++ go01/ex07/vendor/piscine/strrev.go
@@ -1,12 +1,19 @@
 package piscine
 
+func StrLen(s string) int {
+	length := 0
+	runes := []rune(s)
+	for range runes {
+		length++
+	}
+	return length
+}
+
 func StrRev(s string) string {
 	runes := []rune(s)
-	length := len(runes)
+	length := StrLen(s)
 	for i := 0; i < length/2; i++ {
-		tmp := runes[i]
-		runes[i] = runes[length-1-i]
-		runes[length-1-i] = tmp
+		runes[i], runes[length-1-i] = runes[length-1-i], runes[i]
 	}
 	return string(runes)
 }
