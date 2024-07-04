e508bd11fa97e132060549591ca03c2c0669c96f
stakada <stakada@student.42tokyo.jp>
2024-07-04 23:16:10 +0900

    go03: formatting

--- go03/ex03/vendor/piscine/index.go
+++ go03/ex03/vendor/piscine/index.go
@@ -3,9 +3,9 @@ package piscine
 func Index(s string, toFind string) int {
 	runes := []rune(s)
 	toFindRunes := []rune(toFind)
-	for i, _ := range runes {
+	for i := range runes {
 		found := true
-		for j, _ := range toFindRunes {
+		for j := range toFindRunes {
 			if runes[i+j] != toFindRunes[j] {
 				found = false
 				break
