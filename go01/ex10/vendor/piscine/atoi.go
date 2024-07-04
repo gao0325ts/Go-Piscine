742ccf6188c1ea3d5c5014b934a0010712b87531
stakada <stakada@student.42tokyo.jp>
2024-07-04 13:57:42 +0900

    go01/ex10: refactoring

--- go01/ex10/vendor/piscine/atoi.go
+++ go01/ex10/vendor/piscine/atoi.go
@@ -1,22 +1,22 @@
 package piscine
 
 func Atoi(s string) int {
-	var result int
 	sign := 1
-	start := 0
-	num := []rune(s)
-	if num[0] == '-' {
-		sign = -1
-		start++
-	} else if num[0] == '+' {
-		start++
-	}
-	for i := start; i < len(num); i++ {
-		if '0' <= num[i] && num[i] <= '9' {
-			result = result * 10 + int(num[i] - '0')
+	var n int
+	for i, r := range []rune(s) {
+		if i == 0 {
+			if r == '-' {
+				sign = -1
+				continue
+			} else if r == '+' {
+				continue
+			}
+		}
+		if '0' <= r && r <= '9' {
+			n = n*10 + int(r-'0')
 		} else {
 			return 0
 		}
 	}
-	return sign * result
+	return sign * n
 }
