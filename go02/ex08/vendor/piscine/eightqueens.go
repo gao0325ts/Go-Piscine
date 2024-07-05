6c0aa189c41765907b5e63daab3c887144834c83
stakada <stakada@student.42tokyo.jp>
2024-07-05 21:51:10 +0900

    go02/ex08: rename sub-func to lowercase

--- go02/ex08/vendor/piscine/eightqueens.go
+++ go02/ex08/vendor/piscine/eightqueens.go
@@ -4,7 +4,7 @@ import (
 	"ft"
 )
 
-func IsValid(pos []int, row, col int) bool {
+func isValid(pos []int, row, col int) bool {
 	for i := 0; i < row; i++ {
 		if pos[i] == col || pos[i]-col == row-i || col-pos[i] == row-i {
 			return false
@@ -13,7 +13,7 @@ func IsValid(pos []int, row, col int) bool {
 	return true
 }
 
-func FindPatterns(pos []int, row int) {
+func findPatterns(pos []int, row int) {
 	if row == 8 {
 		for i := 0; i < 8; i++ {
 			ft.PrintRune(rune(pos[i] + '1'))
@@ -22,14 +22,14 @@ func FindPatterns(pos []int, row int) {
 		return
 	}
 	for col := 0; col < 8; col++ {
-		if IsValid(pos, row, col) {
+		if isValid(pos, row, col) {
 			pos[row] = col
-			FindPatterns(pos, row+1)
+			findPatterns(pos, row+1)
 		}
 	}
 }
 
 func EightQueens() {
 	var positions [8]int
-	FindPatterns(positions[:], 0)
+	findPatterns(positions[:], 0)
 }
