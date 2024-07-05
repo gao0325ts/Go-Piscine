d969e176affd1b0d45da5b7da804a66adc0a55ed
stakada <stakada@student.42tokyo.jp>
2024-07-05 21:07:18 +0900

    go02/ex04: update main

--- go02/ex05/main.go
+++ go02/ex05/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	fmt.Println(piscine.Sqrt(4))
 	fmt.Println(piscine.Sqrt(3))
-	fmt.Println(piscine.Sqrt(-42))
-	fmt.Println(piscine.Sqrt(-1))
+
+	// --- Additional Tests ---
 	fmt.Println(piscine.Sqrt(0))
 	fmt.Println(piscine.Sqrt(1))
 	fmt.Println(piscine.Sqrt(144))
