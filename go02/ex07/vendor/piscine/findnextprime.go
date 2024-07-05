b1b910fd4e5d9417324047d32ec9de7d0131d269
stakada <stakada@student.42tokyo.jp>
2024-07-05 21:29:54 +0900

    go02/ex07: change var IsPrime

--- go02/ex07/vendor/piscine/findnextprime.go
+++ go02/ex07/vendor/piscine/findnextprime.go
@@ -1,27 +1,27 @@
 package piscine
 
-func FindNextPrime(nb int) int {
-	var IsPrime func(nb int) bool
-	IsPrime = func(nb int) bool {
-		if nb <= 1 {
-			return false
-		} else if nb <= 3 {
-			return true
-		}
-		for i := 2; i <= nb/2; i++ {
-			if nb%i == 0 {
-				return false
-			}
-		}
+func isPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	} else if nb <= 3 {
 		return true
+	} else if nb%2 == 0 || nb%3 == 0 {
+		return false
 	}
+	for i := 5; i <= nb/i; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
 	if nb <= 2 {
 		return 2
 	}
-	for ; ; nb++ {
-		if IsPrime(nb) {
-			return nb
-		}
+	for isPrime(nb) == false {
+		nb++
 	}
 	return nb
 }
