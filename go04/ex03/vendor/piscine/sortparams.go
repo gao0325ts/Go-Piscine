205c968fc339b13a7337f72c8d835e678cee083d
stakada <stakada@student.42tokyo.jp>
2024-07-08 19:29:38 +0900

    add: go04/ex03

---
+++ go04/ex03/vendor/piscine/sortparams.go
@@ -0,0 +1,41 @@
+package piscine
+
+import "ft"
+
+func compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	}
+	return 1
+}
+
+func SortParams(params []string) {
+	var sorted bool
+	for !sorted {
+		sorted = true
+		for i := range params {
+			if i <= 1 {
+				continue
+			}
+			if compare(params[i-1], params[i]) > 0 {
+				params[i-1], params[i] = params[i], params[i-1]
+				sorted = false
+			}
+		}
+	}
+	for i, p := range params {
+		if i == 0 {
+			continue
+		}
+		printStr(p)
+	}
+}
+
+func printStr(s string) {
+	for _, r := range []rune(s) {
+		ft.PrintRune(r)
+	}
+	ft.PrintRune('\n')
+}
