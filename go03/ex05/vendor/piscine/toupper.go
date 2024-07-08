6fac6946dcea42402b0fab64ef82cfed40b78af2
Shorei Takada <ts20040325@gmail.com>
2024-07-08 13:07:26 +0900

    update: improve rune case conversion

--- go03/ex05/vendor/piscine/toupper.go
+++ go03/ex05/vendor/piscine/toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	runes := []rune(s)
 	for i, r := range runes {
 		if r >= 'a' && r <= 'z' {
-			runes[i] -= 32
+			runes[i] -= ('a' - 'A')
 		}
 	}
 	return string(runes)
