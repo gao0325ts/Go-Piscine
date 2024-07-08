1af7ff0526130a793a9b13753aae07adfca0da0f
Shorei Takada <ts20040325@gmail.com>
2024-07-08 13:05:05 +0900

    update: string -> rune comparison

--- go03/ex03/vendor/piscine/index.go
+++ go03/ex03/vendor/piscine/index.go
@@ -11,10 +11,12 @@ func strLen(s string) int {
 func Index(s string, toFind string) int {
 	sLen := strLen(s)
 	toFindLen := strLen(toFind)
+	sRunes := []rune(s)
+	toFindRunes := []rune(toFind)
 	for i := 0; i <= sLen-toFindLen; i++ {
 		found := true
 		for j := 0; j < toFindLen; j++ {
-			if s[i+j] != toFind[j] {
+			if sRunes[i+j] != toFindRunes[j] {
 				found = false
 				break
 			}
