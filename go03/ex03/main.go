80b16581d47c6dc0ca650336f976112e7860c7ab
stakada <stakada@student.42tokyo.jp>
2024-07-09 10:05:11 +0900

    update: add new testcases

--- go03/ex03/main.go
+++ go03/ex03/main.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Println(piscine.Index("Hello!", "l"))
 	fmt.Println(piscine.Index("Salut!", "alu"))
 	fmt.Println(piscine.Index("Ola!", "hOl"))
+
+	// --- Additional Tests ---
+	fmt.Println(piscine.Index("nihao", "o"))
+	fmt.Println(piscine.Index("Bonjour", "jou"))
 }
